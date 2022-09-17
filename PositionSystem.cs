dba3c5a6396b1e15015eefe1aa466426b0f16da2
Cameron MacFarland <distantcam@gmail.com>
2022-09-17 17:15:48 +0800

    Minimal example

---
+++ PositionSystem.cs
@@ -0,0 +1,22 @@
+﻿using DefaultEcs.System;
+
+namespace SortedEntityExample;
+
+public struct PositionInfo
+{
+    public int X;
+    public int Y;
+}
+
+public sealed partial class PositionSystem : AEntitySortedSetSystem<int, PositionInfo>, IComparer<PositionInfo>
+{
+    public int Compare(PositionInfo x, PositionInfo y)
+    {
+        return x.X.CompareTo(y.X);
+    }
+
+    [Update]
+    private void Update(int state, in PositionInfo positionInfo)
+    {
+    }
+}
